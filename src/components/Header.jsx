387f898e8f2979c0501ff341594d3869b3fd101d
Rombas <RomanBash@gmail.com>
2021-02-01 11:11:00 +0300

    feat: create basic app structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/ru/c/cf/Лобос_УПНФМ_%28лого%29.png' />
+    </header>
+)};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'></img>
+            <div>
+                <img src="https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/original.jpeg"></img>
+            </div>
+            <div>
+                My posts
+          <div>
+                    New post
+          </div>
+                <div>
+                    Post 1
+          </div>
+                <div>
+                    Post 2
+          </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
