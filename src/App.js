52e5600e6fb7277700b810f94db0c3d8eee81ac3
RomanBaraban <Romanbash@gmail.com>
2021-02-01 09:37:51 +0300

    init: download and install basic react application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header.js';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      Simple HTML
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div>
+      <div> Hello</div>
+      <div>I'm header</div>
+    </div>
+  );
+}
+
+export default Header;
