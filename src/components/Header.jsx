4df276363bbf9fd4b03058814f3a8bda2f49db96
Rombas <RomanBash@gmail.com>
2021-02-09 10:54:41 +0300

    14 css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import cl from './Header.module.css';
+
 const Header = () => {
     return (
-    <header className='header'>
+    <header className={cl.header}>
         <img src='https://upload.wikimedia.org/wikipedia/ru/c/cf/Лобос_УПНФМ_%28лого%29.png' />
     </header>
 )};
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
+import c from'./Profile.module.css';
+
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={c.content}>
             <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'></img>
             <div>
                 <img src="https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/original.jpeg"></img>
