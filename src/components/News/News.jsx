b9d35611ff6a7f85b9c9d85c216f7822e6ded0c7
RomanBaraban <Romanbash@gmail.com>
2021-02-10 11:37:51 +0300

    19 feat: add react-routrer-dom functionality

---
+++ src/components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import с from './News.module.css';
+
+const News = (props) => {
+    return (
+        <div>
+            News
+        </div>
+    )
+}
+
+export default News;
