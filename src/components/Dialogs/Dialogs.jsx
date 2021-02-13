5d427a4445dde8592e4274f7eff69ba3b996f64f
RomanBaraban <Romanbash@gmail.com>
2021-02-13 11:37:58 +0300

    27 feat: BLL + UI logic impelement. All data moved to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,25 +5,8 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    const dialogs = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Sveta'},
-        {id: 3, name: 'Sasha'},
-        {id: 4, name: 'Piotr'},
-        {id: 5, name: 'Roman'},
-        {id: 6, name: 'Helga'},
-    ];
-    const messageData = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'Hello'},
-        {id: 3, message: 'HiHi'},
-        {id: 4, message: 'Piotr'},
-        {id: 5, message: 'Roman'},
-        {id: 6, message: 'Helga'},
-    ];
-
-    const dialogItems = dialogs.map(item => <DialogItem name={item.name} id={item.id}/>);
-    const messagesItems = messageData.map(item => <Message message={item.message} id={item.id}/>);
+    const dialogItems = props.dialogs.map(item => <DialogItem name={item.name} id={item.id}/>);
+    const messagesItems = props.messages.map(item => <Message message={item.message} id={item.id}/>);
 
     return (
         <div className={s.dialogs}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,11 @@ import c from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import React from "react";
 
-const Profile = () => {
-    const posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It is my first postsElements', likesCount:20},
-    ];
+const Profile = (props) => {
     return (
         <div className={c.content}>
             <ProfileInfo/>
-            <MyPosts posts={posts}/>
+            <MyPosts posts={props.posts}/>
         </div>
     )
 };
