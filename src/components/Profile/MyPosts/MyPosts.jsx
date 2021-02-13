26d6b1f06af0e355f8bd4de7a6db03c4b86eb669
RomanBaraban <Romanbash@gmail.com>
2021-02-13 11:21:11 +0300

    26 feat: change project structure

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,9 @@ import c from'./MyPosts.module.css';
 import Post from './Post/Post';
 import React from "react";
 
-const MyPosts = () => {
-    const posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It is my first postsElements', likesCount:20},
-    ];
-    const postsElements = posts.map(p => <Post message={p.message} likeCount={p.likesCount}/>)
+const MyPosts = (props) => {
+
+    const postsElements = props.posts.map(p => <Post message={p.message} likeCount={p.likesCount}/>)
     return (
         <div>
             {postsElements}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import MyPosts from './MyPosts/MyPosts';
 import c from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import React from "react";
 
 const Profile = () => {
+    const posts = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It is my first postsElements', likesCount:20},
+    ];
     return (
         <div className={c.content}>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </div>
     )
 };
