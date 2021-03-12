2bc2c9d5fa34a0cea85dc0e4e8380c4230755570
RomanBaraban <Romanbash@gmail.com>
2021-03-12 11:13:28 +0300

    47 feat: refactor profile reducer

--- src/index.js
+++ src/index.js
@@ -6,19 +6,12 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 
-const rerenderEntirePage = (state) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-rerenderEntirePage(store.getState());
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntirePage(state);
-});
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -11,18 +11,23 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             const newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             }
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_TEXT_AREA:
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_TEXT_AREA: {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
