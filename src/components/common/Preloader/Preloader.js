91b47628d853d08f2554e25675b600f6bb128246
RomanBaraban <Romanbash@gmail.com>
2021-06-30 21:39:52 +0300

    Updates

--- src/components/common/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import preloader from "../../../assets/images/grid.svg";
-import React from "react";
-
-let Preloader = (props) => {
-    return <img src={preloader} alt={'preloader'}/>
-}
-
-export default Preloader;
